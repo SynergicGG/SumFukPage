f34227822ecd93b300b7939a2ceaef145686fe6d
SynergicGG <43203051+SynergicGG@users.noreply.github.com>
2018-09-26 11:23:08 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# SumFukPage
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,8 @@
+document.getElementById("app").innerHTML = `
+<h1>Hello fellow bird lovers!</h1>
+<div>
+  Find full video
+  <a href="https://www.youtube.com/watch?v=-TcLxlkc2pA" target="_blank" style="color: #01FF70" rel="noopener noreferrer">here</a>
+  if you feel like a bird.
+</div>
+`;
